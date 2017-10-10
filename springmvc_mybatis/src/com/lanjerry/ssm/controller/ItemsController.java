cbcabf6285c0a3015e70e3e12e25c327755de77e
lanjerry <38046851@qq.com>
2017-10-10 20:06:26 +0800

    调试ssm整合项目

--- springmvc_mybatis/src/com/lanjerry/ssm/controller/ItemsController.java
+++ springmvc_mybatis/src/com/lanjerry/ssm/controller/ItemsController.java
@@ -2,6 +2,7 @@ package com.lanjerry.ssm.controller;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -12,17 +13,24 @@ import com.lanjerry.ssm.service.ItemsService;
 @Controller
 public class ItemsController {
 
+	@Autowired
 	private ItemsService itemsService;
-	
+
+	// 商品查询列表
+	// @RequestMapping实现映射器的映射url地址，一般建议讲url和方法名写成一样
 	@RequestMapping("/queryItems")
 	public ModelAndView queryItems() throws Exception {
 
-		List<ItemsCustom> itemList = itemsService.findItemsList(null);
+		List<ItemsCustom> itemsList = itemsService.findItemsList(null);
 
 		ModelAndView modelAndView = new ModelAndView();
-		modelAndView.addObject("itemList", itemList);
+
+		// 相当于request的setAttribut，在jsp页面中通过itemsList取数据
+		modelAndView.addObject("itemsList", itemsList);
+
+		// 指定视图
 		modelAndView.setViewName("items/itemsList");
+
 		return modelAndView;
 	}
-
 }
